3cdb171acd1191cbcd5629bc59a99081d4ca9668
jakazzy <jakazzy@gmail.com>
2020-09-02 16:43:15 +0000

    add charting library

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Nav } from "./styled-components";
+import "./App.css";
+import config from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetid}/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        let batchRowValues = data.valueRanges[0].values;
+
+        const rows = [];
+        for (let i = 1; i < batchRowValues.length; i++) {
+          let rowObject = {};
+          for (let j = 0; j < batchRowValues[i].length; j++) {
+            rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+          }
+          rows.push(rowObject);
+        }
+        this.setState({ items: rows });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <Nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
+          <div className="navbar-brand h1 mb-0 text-large font-medium">
+            Online Retail Dashboard
+          </div>
+          <div className="navbar-nav ml-auto">
+            <div className="user-detail-section">
+              <span className="pr-2">Hi, Sean</span>
+              <span className="img-container">
+                <img src="" className="rounded-circle" alt="user" />
+              </span>
+            </div>
+          </div>
+        </Nav>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+import "dotenv/config";
+
+export default {
+  apiKey: process.env.REACT_APP_APIKEY,
+  spreadsheetid: process.env.REACT_APP_SPREADSHEETID,
+};
